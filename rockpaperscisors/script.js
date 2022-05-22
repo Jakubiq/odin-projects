d7f11f2bc839f94fd994b28c6dc9cadd8bcb2277
jakubiq <jakub.martinec99@gmail.com>
2022-05-22 20:25:06 +0200

    Project done

--- rockpaperscisors/script.js
+++ rockpaperscisors/script.js
@@ -1,3 +1,50 @@
-function choice() {
-    console.log("ahoj")
+let yourScore = 0;
+let compScore = 0;
+
+function play(choose) {
+  var choices = ["rock", "paper", "scissors"];
+  var rnd = Math.floor(Math.random()*3);
+
+  let compChoice = choices[rnd];
+  let result;
+
+  
+
+  if(choose == compChoice){
+    result = "Draw!";
   }
+  if(choose == "rock" && compChoice == "scissors"){
+    yourScore++;
+    result = "You win!";
+  }
+  if(choose == "rock" && compChoice == "paper"){
+    compScore++;
+    result = "You lose :(";
+  }
+  if(choose == "scissors" && compChoice == "rock"){
+    compScore++;
+    result = "You lose :(";
+  } 
+  if(choose == "scissors" && compChoice == "paper"){
+    yourScore++;
+    result = "You win!";
+  }
+  if(choose == "paper" && compChoice == "scissors"){
+    compScore++;
+    result = "You lose :(";
+  }
+  if(choose == "paper" && compChoice == "rock"){
+    yourScore++;
+    result = "You win!";
+  }
+
+
+  console.log(result);
+  document.getElementById('results').style.display = 'block';
+  document.getElementById('results').textContent = result;
+  
+
+  document.getElementById('yourScore').textContent = "You: " + yourScore;
+  document.getElementById('compScore').textContent = "Computer: " + compScore;
+
+}
